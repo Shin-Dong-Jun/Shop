edfec8c2de94827e2719f54574842eaac8eac729
jihaneol <6531z@naver.com>
2024-12-14 19:26:15 +0900

    init project - jdk 21 - 스프링 부트 설정

---
+++ bravobra/src/main/java/com/example/bravobra/BravobraApplication.java
@@ -0,0 +1,13 @@
+package com.example.bravobra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BravobraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BravobraApplication.class, args);
+    }
+
+}
---
+++ bravobra/src/test/java/com/example/bravobra/BravobraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bravobra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BravobraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
