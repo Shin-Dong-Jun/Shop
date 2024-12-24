e3962b2c64ca59f787876d9a5f573b65249c8805
jihaneol <6531z@naver.com>
2024-12-24 14:49:59 +0900

    ➕ Feat(feature/cart): 장바구니 화면, 해더 추가

--- bravobra/src/main/java/com/example/bravobra/controller/OrderController.java
+++ bravobra/src/main/java/com/example/bravobra/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.bravobra.controller;
 import com.example.bravobra.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/order")
 public class OrderController {
     private final OrderService orderService;
+
+    @PostMapping
+    public String order(Long cartId) {
+        System.out.println(cartId);
+        return "redirect:/cart/list";
+    }
 
 }
